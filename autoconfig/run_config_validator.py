fbfd4244e378355e0afdbd528dafdcb79bdf4bf3
Tanveer Singh <tanveer.singh@databricks.com>
2025-07-09 09:38:48 +0530

    1. updated autoconfig 2. Added sample ado pipeline

--- autoconfig/run_config_validator.py
+++ autoconfig/run_config_validator.py
@@ -2,14 +2,13 @@ import argparse
 import json
 from argparse import ArgumentParser
 
-from databricks.sdk import WorkspaceClient
-
 from autoconfig.config_validator import ConfigValidator
 
 parser = argparse.ArgumentParser(description='Validate Unity Catalog configuration')
 parser.add_argument('--config_dir', type=str,required=True, help='Directory Path where .yml/.yaml configuration files are located')
 parser.add_argument("--workspace_url",type=str,required=True, help = "Databricks workspace URL, i.e. https://adb-xxx.x.azuredatabricks.net" )
 parser.add_argument("--subscription_id",type=str,required=True, help = "Azure subscription ID, i.e. xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" )
+
 
 
 def main():
@@ -20,7 +19,9 @@ def main():
                           workspace_url = args.workspace_url
                         )
     l_errors = cvl.validate()
-    print(json.dumps(l_errors, indent=4))
+    if l_errors:
+        e = Exception("Error encountered during validation of configs: "+json.dumps(l_errors, indent=4))
+        raise e
 
 
 if __name__ == "__main__":
